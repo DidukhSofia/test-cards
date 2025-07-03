07934300d647a0aca5f567f3ce36e48e0ab84765
User <diduhs0@gmail.com>
2025-07-03 21:55:47 +0300

    Add pagination

--- src/components/ListCards/ListCards.jsx
+++ src/components/ListCards/ListCards.jsx
@@ -1,71 +1,56 @@
-// import { useEffect, useState } from "react";
-// import { db } from "../../services/firebase"; 
-// import { collection, getDocs } from "firebase/firestore";
-// import Card from "../Card/Card";
-// import styles from "./listCards.module.scss"
-// function ListCards() {
-//   const [cards, setCards] = useState([]);
-
-//   useEffect(() => {
-//     const fetchCards = async () => {
-//       try {
-//         const querySnapshot = await getDocs(collection(db, "Cards"));
-//         const data = querySnapshot.docs.map(doc => ({
-//           id: doc.id,
-//           ...doc.data(),
-//         }));
-//         setCards(data);
-//       } catch (error) {
-//         console.error("Error retrieving data:", error);
-//       }
-//     };
-
-//     fetchCards();
-//   }, []);
-
-//   return (
-//     <ul className={styles.list}>
-//       {cards.map(card => (
-//         <li key={card.id}>
-//             <Card name={card.name} prize={card.prize} field={card.field} description={card.description} />
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// }
-
-// export default ListCards;
-
-
+import { useState } from "react";
 import { useCards } from "../../context/CardContext";
 import Card from "../Card/Card";
+import PaginationControls from "../Pagination/Pagination";
 import styles from "./listCards.module.scss";
 
 function ListCards() {
   const { cards, priceRange, loading } = useCards();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(8);
 
   const [minPrice, maxPrice] = priceRange;
 
   if (loading) return <p>Завантаження карток...</p>;
 
-  const filteredCards = cards.filter(card => {
+  const filteredCards = cards.filter((card) => {
     const price = Number(card.prize);
     return price >= minPrice && price <= maxPrice;
   });
 
+  const totalPages = Math.ceil(filteredCards.length / pageSize);
+  const paginatedCards = filteredCards.slice(
+    (currentPage - 1) * pageSize,
+    currentPage * pageSize
+  );
+
   return (
-    <ul className={styles.list}>
-      {filteredCards.map((card) => (
-        <li key={card.id}>
-          <Card
-            name={card.name}
-            prize={card.prize}
-            field={card.field}
-            description={card.description}
-          />
-        </li>
-      ))}
-    </ul>
+    <>
+    <PaginationControls
+        totalPages={totalPages}
+        currentPage={currentPage}
+        onPageChange={(_, page) => setCurrentPage(page)}
+        pageSize={pageSize}
+        onPageSizeChange={(newSize) => {
+          setPageSize(newSize);
+          setCurrentPage(1);
+        }}
+      />
+      <ul className={styles.list}>
+        {paginatedCards.map((card) => (
+          <li key={card.id}>
+            <Card
+              name={card.name}
+              prize={card.prize}
+              field={card.field}
+              description={card.description}
+            />
+          </li>
+        ))}
+      </ul>
+
+      
+    </>
   );
 }
 
---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,64 @@
+import Pagination from "@mui/material/Pagination";
+import PaginationItem from "@mui/material/PaginationItem";
+import TextField from "@mui/material/TextField";
+import styles from "./pagination.module.scss"
+
+function PaginationControls({
+  totalPages,
+  currentPage,
+  onPageChange,
+  pageSize,
+  onPageSizeChange,
+}) {
+ const maxResultsOfPage = 10;
+  const handleInputChange = (e) => {
+    let value = Number(e.target.value);
+    if (isNaN(value)) return;
+    if (value < 1) value = 1;
+    if (value > maxResultsOfPage) value = maxResultsOfPage;
+    onPageSizeChange(value);
+  };
+
+  if (totalPages <= 1) return null;
+
+  return (
+    <div className={styles.pagination}>
+      <Pagination
+        count={totalPages}
+        page={currentPage}
+        onChange={onPageChange}
+        variant="outlined"
+        shape="rounded"
+        showFirstButton
+        showLastButton
+        renderItem={(item) => (
+          <PaginationItem
+            {...item}
+            components={{
+              first: () => <span>First</span>,
+              last: () => <span>Last</span>,
+              previous: () => <span>Prev</span>,
+              next: () => <span>Next</span>,
+            }}
+          />
+        )}
+      />
+
+      <div className={styles.pagination__counter}>
+        <p className={styles.pagination__counter_text}>Results per page:</p>
+        <TextField
+          type="number"
+          value={pageSize}
+          size="small"
+          inputProps={{
+            min: 1,
+            max: maxResultsOfPage,
+          }}
+          onChange={handleInputChange}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default PaginationControls;

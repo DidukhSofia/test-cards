eec31afb1fffe43b3db46d7198f2cfd8aa372a37
User <diduhs0@gmail.com>
2025-07-04 21:34:21 +0300

    Add ordering

---
+++ src/components/Ordering/Ordering.jsx
@@ -0,0 +1,24 @@
+import styles from "./ordering.module.scss";
+
+function Ordering({ sortOrder, onSortChange }) {
+  return (
+    <div className={styles.ordering}>
+      <span className={styles.ordering__text}>Order by:</span>
+      <div className={styles.ordering__btns}>
+      <button
+        className={`${styles.ordering__button} ${sortOrder === "priceAsc" ? styles.ordering__active : ""}`}        onClick={() => onSortChange("priceAsc")}
+      >
+        Lowest price
+      </button>
+      <button
+        className={`${styles.ordering__button} ${sortOrder === "newest" ? styles.ordering__active : ""}`}
+        onClick={() => onSortChange("newest")}
+      >
+        Newest Listings
+      </button>
+      </div>
+    </div>
+  );
+}
+
+export default Ordering;
